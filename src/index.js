959ff651ff17bb532a95a36892cd5d49ddb0738a
Blayze <blayzestone@protonmail.com>
2020-10-07 16:53:47 -0700

    setup redux store to manage future global state

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import 'antd/dist/antd.less';
 
+import reducer from './state/reducers/';
 import { Loading } from './components/common/Loading';
 import NavBar from './components/common/NavBar';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
