5ac8c8f32491134c5b230515804866dc8c9da63e
MaryamMosstoufi <maryam@zooxoos.com>
2020-11-08 07:51:25 -0500

    Create Mapbox

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -4,6 +4,7 @@ import Stats from '../pages/Stats';
 import 'antd/dist/antd.css';
 import '../../styles/index.css';
 import { Tabs, Button, Popover } from 'antd';
+import Mapbox from './Mapbox';
 
 export const Loading = () => {
   const { TabPane } = Tabs;
@@ -19,6 +20,9 @@ export const Loading = () => {
           </TabPane>
           <TabPane tab="Stats" key="2" style={{ backgroundColor: '#191a1a' }}>
             <div id="graph">{<Stats />}</div>
+          </TabPane>
+          <TabPane tab="Mapbox">
+            <Mapbox />
           </TabPane>
         </Tabs>
       </main>
