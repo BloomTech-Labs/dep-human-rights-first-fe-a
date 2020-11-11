98a186fbfc76e076cd392b832cc91a89927e691d
antonio martinez baez <tono.mtzb@gmail.com>
2020-11-11 17:43:39 -0600

    testing husky

--- src/components/pages/Incidents/IncidentsPage.js
+++ src/components/pages/Incidents/IncidentsPage.js
@@ -8,7 +8,7 @@ import { useIncidents } from '../../../hooks/useIncidentsPaginated';
 const IncidentsPage = () => {
   const incidents = useIncidents();
 
-  const [page, setPage] = useState(0);
+  const[page,setPage] =useState(0);
 
   // const [itemsPerPage, setItemsPerPage] = useState(24);
   // const [page, setPage] = useState('/incidents/?page=1');
@@ -19,6 +19,8 @@ const IncidentsPage = () => {
   // );
   // const [currentPage, setCurrentPage] = useState();
   // const [pageContent, setPageContent] = useState();
+
+  // testsing files
 
   return (
     <section className="uk-section uk-section-small">
@@ -39,11 +41,9 @@ const IncidentsPage = () => {
               })}
         </ul>
       </div>
-      <button onClick={() => setPage(old => old - 1)}> Previous</button>
-      <button onClick={() => setPage(old => old + 1)}> Next</button>
-      <span>
-        Current page: {page + 1} {incidents.isFetching ? '...' : ''}
-      </span>
+      <button onClick={()=> setPage(old => old-1)}> Previous</button>
+      <button onClick={()=> setPage(old => old+1)}> Next</button>
+      <span>Current page: {page + 1} {incidents.isFetching ? '...' : ''}</span>
       {/* <Pagination
         prevPage={prevPage}
         nextPage={nextPage}
