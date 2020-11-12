04ddd75957059288fd583be9ab53a30abe581b1d
MaryamMosstoufi <maryam@zooxoos.com>
2020-11-11 19:58:12 -0500

    Create Fatality Page

---
+++ src/components/pages/Fatality/FatalityPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const FatalityPage = () => {
+  return <div></div>;
+};
+export default FatalityPage;
---
+++ src/components/pages/Fatality/index.js
@@ -0,0 +1,1 @@
+export { default as FatalityPage } from './FatalityPage';
