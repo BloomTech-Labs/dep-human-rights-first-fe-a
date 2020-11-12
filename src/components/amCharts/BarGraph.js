d2ec6f531d825cfa203d309637a827e52e3d7d30
Ash <ashley.bergsma42@gmail.com>
2020-11-12 16:47:31 -0700

    Sets up barGraph for drop down

--- src/components/amCharts/BarGraph.js
+++ src/components/amCharts/BarGraph.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
-import am4themes_animated from '@amcharts/amcharts4/themes/animated';
-
-am4core.useTheme(am4themes_animated);
 
 const nationalData = [
   {
