3218fb7810a7b46f338b63f3fc25c3fa3708cb9d
MaryamMosstoufi <maryam@zooxoos.com>
2020-11-12 08:45:52 -0500

    Map Fatalities

--- src/components/pages/Fatality/FatalityPage.js
+++ src/components/pages/Fatality/FatalityPage.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import {fatality} from '../../../data/fatality';
 
 const FatalityPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      {fatality.isLoading
+        ? 'Loading...'
+        : fatality.map(victim => {
+          return (
+            <div>test</div>
+          );
+        })
+      }
+    </div> 
+  )
+  
+  
 };
 export default FatalityPage;
