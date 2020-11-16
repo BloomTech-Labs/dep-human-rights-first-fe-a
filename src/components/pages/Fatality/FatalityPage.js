e5bb1035ab7d56a5e4d95761b0c0711850ea7c09
MaryamMosstoufi <maryam@zooxoos.com>
2020-11-16 11:54:31 -0500

    Use MPV Live Data

--- src/components/pages/Fatality/FatalityPage.js
+++ src/components/pages/Fatality/FatalityPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import FatalityCard from '../../common/FatalityCard';
 
-import { fatality } from '../../../data/fatality';
+import fatality from '../../../data/mpv/fatalityList.json'
+
 
 const FatalityPage = () => {
   let victim_id = 0;
+  let currentPage = fatality.slice(500, 512)
+  console.log(currentPage)
   return (
     <section className="uk-section uk-section-small">
       <div className="uk-container uk-container-expand">
@@ -13,12 +16,11 @@ const FatalityPage = () => {
           className="uk-grid-small uk-child-width-1-2@s uk-child-width-1-4@m"
           data-uk-grid="masonry: true"
         >
-          {fatality.isLoading
-            ? 'Loading...'
-            : fatality.map(victim => {
+          {currentPage.map(victim => {
                 victim_id += 1;
                 return <FatalityCard key={victim_id} victim={victim} />;
-              })}
+            })
+          }
         </ul>
       </div>
     </section>
