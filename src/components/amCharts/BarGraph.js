0b2b306fc53f7f184a340ecb38df8e90d425157f
Ash <ashley.bergsma42@gmail.com>
2020-11-18 10:13:51 -0700

    Adds comments and readme

--- src/components/amCharts/BarGraph.js
+++ src/components/amCharts/BarGraph.js
@@ -8,15 +8,19 @@ import importData from '../../data/wapo/byState.json';
 am4core.useTheme(am4themes_animated); 
 
 function BarGraph() {
+  // the ref is set for proper disposal of a chart after the component unmounts 
   const chart = useRef(null);
   const barData = Object.entries(importData); 
 
   const preparedData = []; 
 
+  //* Note: This data was created by a group member using python. I have pulled it in and filtered it here to organize the data 
   barData.forEach(function(item){
     preparedData.push({ state: item[0], number: item[1].length })
   })
-  
+
+  //* Note: The amCharts docs don't contain much information on amCharts in React, I first tried to get these charts to work in a useEffect, but wasn't able to
+  // useLayoutEffect runs synchronously, after DOM manipulations (allowing the library to perform DOM measurements for the charts)
   useLayoutEffect(() => {
     //* Creates chart instance
     let barGraph = am4core.create('barGraph', am4charts.XYChart);
@@ -39,7 +43,7 @@ function BarGraph() {
     //* This is the first Y-axis 
     let valueAxis1 = barGraph.yAxes.push(new am4charts.ValueAxis());
 
-    //* This is a secondary axis, the goal was to use this goal to comparatively view data. I didn't get there, but if a certain future dev came along...
+    //* This is a secondary axis, the goal was to use this goal to comparatively view data (two colums series over one another). I didn't get there, but if a certain future dev came along...
     let valueAxis2 = barGraph.yAxes.push(new am4charts.ValueAxis());
     valueAxis2.renderer.opposite = true;
     valueAxis2.renderer.grid.template.disabled = true;
@@ -55,17 +59,8 @@ function BarGraph() {
     series1.clustered = false;
     series1.columns.template.width = am4core.percent(75);
 
-    // let series2 = barGraph.series.push(new am4charts.ColumnSeries());
-    // series2.dataFields.valueY = 'local';
-    // series2.dataFields.categoryX = 'type';
-    // series2.yAxis = valueAxis1;
-    // series2.name = 'Locally Reported Incidents';
-    // series2.fill = barGraph.colors.getIndex(1).lighten(0.5);
-    // series2.strokeWidth = 0;
-    // series2.clustered = false;
-    // series2.toBack();
-
     //* Adds a cursor
+    // the cursor can be set to hidden - but you should keep it for zoom capabilities
     barGraph.cursor = new am4charts.XYCursor();
 
     //* Adds a scrollbar - that box below the graph that you can drag to view a subset of the graph
